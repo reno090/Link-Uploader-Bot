fc58c346c6569da67773debc50cc2ea1deb6b1d3
AL-Noman-21 <86485726+AL-Noman-21@users.noreply.github.com>
2022-01-29 13:56:15 +0600

    Create youtube_dl_echo.py

--- Atrocious_Robot_2/modules/helper_funcs/url_uploader_helper/plugins/youtube_dl_echo.py
+++ Atrocious_Robot_2/modules/helper_funcs/url_uploader_helper/plugins/youtube_dl_echo.py
@@ -24,7 +24,8 @@ from pyrogram.errors import UserNotParticipant
 
 DOWNLOAD_START = "Now Downloading.."
 FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-
+SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | filename | username | password"""
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
